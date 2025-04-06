f63cf7a368849e68884dd7e43129b142a4c8c459
Satya <satyarths.ug23.ec@nitp.ac.in>
2025-04-06 21:24:00 +0530

    Blcokchain Finals

--- frontend/blockchain/truffle-config.js
+++ frontend/blockchain/truffle-config.js
@@ -1,3 +1,9 @@
+//importing the private key from the .env file
+require('dotenv').config();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const END_POINT = process.env.SEPOLIA_INFURA_URL;
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +12,10 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(PRIVATE_KEY, END_POINT),
+      network_id: 11155111
     }
   }
   
